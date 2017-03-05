92ff42fb7d715dfa09f0006f38776ac60be96a4b
Vitaliy Franchook <vfranchook@gmail.com>
2017-03-05 04:46:49 +0200

    removed unnecessary prepare methon from telegram handler

--- handlers/main.py
+++ handlers/main.py
@@ -20,18 +20,6 @@ class TelegramHandler(RequestHandler):
         # disable XSRF checking for this handler
         pass
 
-    async def prepare(self):
-        """
-        Dispatch request to proper bot.
-        """
-        bot_id = self.path_kwargs['bot_id']
-        bot = self.application.bots.get(bot_id, None)
-        if bot is not None:
-            self.bot = bot
-        else:
-            self.set_status(200)
-            self.finish()
-
     async def post(self, *args, **kwargs):
         data = json_decode(self.request.body)
         message = data['message']['text']
