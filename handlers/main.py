87fd4ffd8ea531dfdb2f4dd4245627e298345339
Vitaliy Franchook <vfranchook@gmail.com>
2017-03-05 07:21:39 +0200

    added context

--- handlers/main.py
+++ handlers/main.py
@@ -17,6 +17,7 @@ class MainHandler(RequestHandler):
 
 
 class TelegramHandler(RequestHandler):
+    CONTEXT = {}
     CACHE = {}
 
     def check_xsrf_cookie(self):
@@ -32,24 +33,38 @@ class TelegramHandler(RequestHandler):
         response = None
         if 'callback_query' not in data:
             conversation_id = data['message']['chat']['id']
-            message = data['message']['text']
-            response = self.application.bot.respond_faq(message)
-            message_type = 'answer' if type(response) in [tuple, str] else 'questions'
-            if message_type == 'questions':
-                # save to cache
-                self.CACHE[conversation_id] = list(response.values())
-            else:
-                if conversation_id in self.CACHE:
-                    self.CACHE.pop(conversation_id)
-
         else:
             conversation_id = data['callback_query']['message']['chat']['id']
-            message = data['callback_query']['data']
-            index = int(message)
 
-            cached = self.CACHE.get(conversation_id, None)
-            if cached is not None:
-                response = cached[index]
-                message_type = 'answer'
-        if response is not None:
-            await telegram.send(conversation_id, response, message_type)
+        if conversation_id in self.CONTEXT:
+            context = self.CONTEXT[conversation_id]
+        else:
+            context = {
+                'state': 'main'
+            }
+            self.CONTEXT[conversation_id] = context
+
+        if context['state'] == 'main':
+
+            if 'callback_query' not in data:
+                message = data['message']['text']
+                response = self.application.bot.respond_faq(message)
+
+                message_type = 'answer' if type(response) in [tuple, str] else 'questions'
+                if message_type == 'questions':
+                    # save to cache
+                    self.CACHE[conversation_id] = list(response.values())
+                else:
+                    if conversation_id in self.CACHE:
+                        self.CACHE.pop(conversation_id)
+
+            else:
+                message = data['callback_query']['data']
+                index = int(message)
+
+                cached = self.CACHE.get(conversation_id, None)
+                if cached is not None:
+                    response = cached[index]
+                    message_type = 'answer'
+            if response is not None:
+                await telegram.send(conversation_id, response, message_type)
